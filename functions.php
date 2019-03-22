e1d88657c6ac8b616078631b17e42b91ae48e5f5
aberoud <adrien.beroud@hotmail.com>
2019-03-22 16:47:04 +0100

    Ajout de la font Courier Prime

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 // @ https://digwp.com/2016/01/include-styles-child-theme/
 function id361_style_parent() {
 	// enqueue parent styles
-	wp_enqueue_style('uku-parent-theme', get_template_directory_uri() .'/style.css');
+	wp_enqueue_style('atomic-parent-theme', get_template_directory_uri() .'/style.css');
 }
 add_action('wp_enqueue_scripts', 'id361_style_parent');
 
